17f446e3cf7c4c859c9090bf1911fed5ab8e2f10
Ilelwa <bonkereinhardt36@gmail.com>
2022-10-20 22:41:35 +0300

    styling using jss

--- style-react-component/style-react-component/src/components/Alert/Alert.js
+++ style-react-component/style-react-component/src/components/Alert/Alert.js
@@ -1,10 +1,29 @@
-import './Alert.css';
 import PropTypes from 'prop-types';
+import { createUseStyles } from 'react-jss';
+//type colors
+const colors = {
+    success: '#6da06f',
+    error: '#f56260'
+};
+//create jss styles it will take the color based on type 
+const styles = createUseStyles({
+    wrapper :{
+        padding:15,
+        marginBottom:15,
+        border :({type}) => `${colors[type]} solid 1px`, 
+        '& > h2':{
+            color:({type}) => colors[type],
+            margin :[0,0,10,0],
+        }
+    }
 
+})
 export default function Alert({type,title,children}) {
     // call the right borders/style as per type
+    const styling = styles({type})
+
     return(
-        <div className={`alert-wrapper ${type}`}> 
+        <div className={styling.wrapper}> 
             <h2>{title}</h2>
             {children}
         </div>
--- style-react-component/style-react-component/src/components/App/App.js
+++ style-react-component/style-react-component/src/components/App/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import CartError from '../CartError/CartError';
 import CartSuccess from '../CartSuccess/CartSuccess';
+import {createUseStyles} from 'react-jss';
+
+//set us the jss style
+const useStyle = createUseStyles({
+  wrapper : {
+    padding:20,
+  }
+})
 function App() {
+  const classes = useStyle();
   return (
-    <div className = 'wrapper'>
+    <div className = {classes.wrapper}>
       <div>Alert</div>
       <CartError/>
       <CartSuccess/>
