20557894cc2d9d1a1b16ea1c0530a7a1207441ef
Ilelwa <bonkereinhardt36@gmail.com>
2022-10-22 07:55:17 +0300

    from useState to useReducer hook

--- state-with-hooks/state-with-hooks/src/components/Product/Product.js
+++ state-with-hooks/state-with-hooks/src/components/Product/Product.js
@@ -1,38 +1,86 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import './Product.css';
 
+const products = [
+    {
+      emoji: '🍦',
+      name: 'ice cream',
+      price: 5
+    },
+    {
+      emoji: '🍩',
+      name: 'donuts',
+      price: 2.5,
+    },
+    {
+      emoji: '🍉',
+      name: 'watermelon',
+      price: 4
+    }
+  ];
+  
 const currencyOptions = {
-    minimumFractionDigit:2,
-    maximumFractionDigit:2,
+    minimumFractionDigits:2,
+    maximumFractionDigits:2,
 }
 //get total function
-function getTotal(total) {
+function getTotal(cart) {
+    const total = cart.reduce((totalCost,item) => totalCost + item.price,0);
     return total.toLocaleString(undefined,currencyOptions)
     
 }
+//this reducer function checks current state if add or remove of name then appends the product end of the array
+function cartReducer(state,action){
+    switch (action.type) {
+        case 'add':
+            return [...state,action.product];
+        case 'remove':
+            const productIndex = state.findIndex(item => item.name === action.product.name);
+            //check if item is in state by findIndex 
+            if (productIndex < 0){
+
+                return state;
+            } 
+
+            const update = [...state];  
+            update.splice(productIndex,1)  
+            return update
+        default:
+            return state;
+
+    }
+}
 
 export default function Product(){
-    const[cart,setCart] = useState([]);
-    const[total,setTotal] = useState(0);
+    //current state
+    const[cart,setCart] = useReducer(cartReducer,[]);
 
     //add function
-    function add(){
-        setCart(['ice cream']);
-        setTotal(5);
+    function add(product){
+        setCart({product,type :'add'});
+
+    }
+
+    //remove function 
+    function remove(product){
+        setCart({product,type:'remove'});
     }
     return(
         <div className="wrapper">
             <div>Shopping cart:{cart.length} items. </div>
-            <div>Total price: {getTotal(total)}</div>
-            <div className="product">
-                <span role='img' aria-label="ice cream">🍦</span> 
-            </div>
-            <button onClick={add}>Add</button>
-            <button onClick={() =>{
-                setCart([]);
-                setTotal(0);
-            }
-            }>Remove</button>
+            <div>Total price: {getTotal(cart)}</div>
+            <div>
+                {products.map(product =>(
+                    <div key={product.name}>
+                        <div className="product">
+                            <span role='img' aria-label={product.name}>{product.emoji}</span> 
+                        </div>
+                        <button onClick={() => add(product)}>Add</button>
+                        <button onClick={() => remove(product)}>Remove</button>    
+                    </div>
+                ))}
+            </div>    
+            
         </div>
     )
 
