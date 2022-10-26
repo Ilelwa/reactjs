53bda6d163064101006532e341d58a0d927bb7cb
Ilelwa <bonkereinhardt36@gmail.com>
2022-10-26 05:09:35 +0300

    uncontrolled components

--- forms-with-jsx/form-with-jsx/src/components/App/App.js
+++ forms-with-jsx/form-with-jsx/src/components/App/App.js
@@ -1,7 +1,16 @@
-import { useState } from 'react';
+import { useState,useReducer } from 'react';
 import './App.css';
+
+const formReducer = (state,event) => {
+  return{
+    ...state,
+    [event.name]:event.value
+  }
+}
+
 function App() {
-  const[submitting,setSubmitting]=useState(false)  
+  const[formData,setFormData] = useReducer(formReducer,{});
+  const[submitting,setSubmitting]=useState(false);  
   const handleSubmit =event =>{
     event.preventDefault();
     setSubmitting(true);
@@ -11,16 +20,57 @@ function App() {
     },3000 )
 
   } 
+  const handleChange = event =>{
+    const isCheckbox = event.target.type === 'checkbox';
+    
+
+    setFormData({
+      name:event.target.name,
+      value: isCheckbox ? event.target.checked : event.target.value,
+    });
+  }
+  
   return (
    <div className='wrapper'>
     {submitting && 
-      <div>Submitting</div>
+      <div>Submitting
+        <ul>
+          {Object.entries(formData).map(([name,value]) =>(
+            <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+          ))}
+        
+        </ul>
+      </div>
+      
     }
     <form onSubmit={handleSubmit}>
       <fieldset>
         <label> 
           <p>Name</p>
-          <input text='name'/>
+          <input name='name' onChange={handleChange}/>
+        </label>
+      </fieldset>
+      <fieldset>
+        <label>
+          <p>Select Apple</p>
+          <select name= 'apples' onChange={handleChange}>
+            <option value=''>Choose an option</option>
+            <option value='Fuji'>Fuji</option>
+            <option value='jonathan'>jonath</option>
+            <option value='honey-crisp'>honey-crisp</option>
+          </select>
+        </label>
+      </fieldset>
+      <fieldset>
+        <label>
+          <p>Count</p>
+          <input type='number' name = 'count' onChange={handleChange} step='1' />
+        </label>
+      </fieldset>
+      <fieldset>
+        <label>
+          <p>Gift wrap</p>
+          <input type='checkbox' name='gift-wrap' onChange={handleChange} />
         </label>
       </fieldset>
       <button>Submit</button>
